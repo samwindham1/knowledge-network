841580d61dcc3eedf9579ab4ff2de0a5da8bdbf3
samwindham1 <sam.windham95@gmail.com>
2019-07-19 17:38:51 +0200

    Update Babel settings to work with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,36 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
   },
   module: {
     rules: [
-      { test: /\.(js)$/, loader: ['babel-loader'] },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] }
     ]
   },
-  mode: 'development',
+  resolve: {
+    alias: {
+      "@data": path.resolve(__dirname, './data/'),
+      "@api": path.resolve(__dirname, './src/api')
+    }
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
-  ]
+  ],
+  node: {
+    fs: 'empty'
+  }
 }
