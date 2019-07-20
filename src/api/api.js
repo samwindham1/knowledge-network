581136f6aba8be795765f43e650a1ad6484a0369
samwindham1 <sam.windham95@gmail.com>
2019-07-20 14:10:38 +0200

    Split domain, network, and schema  in api

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import { promises } from 'fs';
 
 var networkData = require('@data/network.json');
 
-var getData = () => new Promise((resolve) => resolve(networkData));
+var getSchema = () => new Promise((resolve) => resolve(networkData.schema));
+var getDomain = () => new Promise((resolve) => resolve(networkData.domain));
+var getNetwork = () => new Promise((resolve) => resolve(networkData.network));
 
-export default { getData };
+export default { getSchema, getDomain, getNetwork };
